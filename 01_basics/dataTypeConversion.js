030f87c13861582e4e67355eeec3f09675cae4c5
harshl36 <harshlavania36@gmail.com>
2024-06-08 16:12:11 +0530

    Learned Datatype Conversion

---
+++ 01_basics/dataTypeConversion.js
@@ -0,0 +1,16 @@
+// let age = "23";
+
+// console.log(typeof age);
+
+// let valueInNumber = Number(age);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+//"33" -> 33
+// "33abc" -> NaN
+//true ->1, false ->0
+let isLoggedIn =1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
