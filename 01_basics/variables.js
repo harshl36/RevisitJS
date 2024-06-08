96cc0a667fcb6a2880ccd6d54a7e9fe24fc9fa57
harshl36 <harshlavania36@gmail.com>
2024-06-08 15:23:56 +0530

    Learned variable declaration

---
+++ 01_basics/variables.js
@@ -0,0 +1,14 @@
+const accountID = 144553
+let accountemail = "harsh@java.com"
+var accountPassword = "12345"
+accountCity ="Gurgaon"
+
+// accountID = 2
+
+accountemail="hl@rl.com"
+accountPassword="54321"
+accountCity ="Delhi"
+
+
+console.log(accountID);
+console.table([accountID, accountPassword, accountemail, accountCity])
