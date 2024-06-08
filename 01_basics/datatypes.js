2f3514aeba02d35a1932418600df97a35676b2eb
harshl36 <harshlavania36@gmail.com>
2024-06-08 15:36:47 +0530

    Learned data types

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) we are using node.js not browsers
+
+let name ="harsh"
+let age =23
+let isLoggedIn = false
+let state;
+
+
+/*
+number =>2 to power 53
+bigint
+string =>""
+boolean => true/false
+null => stand alone value
+undefined => 
+symbol => unique
+
+object 
+*/
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
