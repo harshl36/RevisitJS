538ddaadfefb280881ef418ae0064933bb816b70
harshl36 <harshlavania36@gmail.com>
2024-06-09 11:11:35 +0530

    Learned Operations

---
+++ 01_basics/Operations.js
@@ -0,0 +1,38 @@
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+// Two strings can be added as shown below
+/*let str1 = "Hello"
+let str2 = " Harsh"
+
+let str3 = str1+str2
+console.log(str3);*/
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + "2" + 2);
+
+// // if the string is gieven at the last then JS will first add starting numbers and the sum will be given with the string as output as shown below
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+
+// let num1, num2, num3
+
+// num1=num2=num3= 2+2
+// console.table([num1,num2,num3])
+
+// Game Counter
+
+// let gameCounter= 100
+// gameCounter++;
+// console.log(gameCounter);
