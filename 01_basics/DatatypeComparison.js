174607daf79375022605c92d3f00b2c3a2b25c02
harshl36 <harshlavania36@gmail.com>
2024-06-09 23:39:48 +0530

    Learned Datatype comparison

---
+++ 01_basics/DatatypeComparison.js
@@ -0,0 +1,26 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// -------------------------------------------------------------------------
+// Comparison of different datatypes
+
+// console.log("2">1);  //string with number
+// console.log("02">1); //string with number
+
+// -------------------------------------------------------------------------
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0); //comparison convert null to a number treating it as 0 
+
+// console.log(undefined ==0); //always gives false
+
+// NOTE:- Comparison and Equality check works differentely in JS
+// AVOID THESE TYPE OF CONVERSION
+// -------------------------------------------------------------------------
+
+// Strict check 
+
+// console.log("2" === 2);
