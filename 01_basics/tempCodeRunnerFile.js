617e99daa6f588ef22ac1a658ba0c32dd990116c
harshl36 <harshlavania36@gmail.com>
2024-06-11 22:21:51 +0530

    tempfile

---
+++ 01_basics/tempCodeRunnerFile.js
@@ -0,0 +1,18 @@
+const name = "Harsh "
+// const repoCount = 50
+
+// // console.log(name + repoCount);
+
+// // Use of Thatis, string interpolation
+
+// // syntax of adding string in console.log is  ${}
+
+// // console.log('My name is ${name} and my repo count is ${repoCount}');
+// console.log(`Hello My name is ${name}and my repo count is ${repoCount}`);
+
+
+// // decalaration of string using object 
+// const gameName = new String('Harsh_fc')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
