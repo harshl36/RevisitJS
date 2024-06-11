508e920dc66b46042bd1c377d08846fca8b42347
harshl36 <harshlavania36@gmail.com>
2024-06-11 22:55:14 +0530

    Learned number and math

---
+++ 01_basics/numbers&maths.js
@@ -0,0 +1,40 @@
+// const score =400
+// console.log(score);
+// const balance= new Number(100)
+// console.log(balance);
+
+
+
+// console.log(balance.toString());
+// console.log(balance.toString().length); // can give length
+
+// const updatedBalance = new Number(400) 
+// console.log(updatedBalance.toFixed(1));   //gives whole number
+
+
+// const otherNumber = 23.8966
+// console.log(otherNumber.toPrecision(3));  //focuses on the given precision value
+
+// const newNum = 1000000
+// console.log(newNum.toLocaleString("en-IN"));
+
+
+// // MATHS
+
+// console.log(Math);
+// console.log(Math.abs(-4)); gives absolute value
+// console.log(Math.round(4.3)); gives round off
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.5));
+
+// console.log(Math.min(4,3,5,2,8)); gives minimum of an array
+// console.log(Math.max(4,3,5,2,8)); gives maximum of an array
+
+// console.log(Math.random());
+// console.log(Math.random()*10);
+const min=1
+const max=6
+console.log(Math.round(Math.abs(6-(Math.random()*10)+1)));
+
+
+console.log(Math.floor(Math.random() * (max-min+1))+min)
