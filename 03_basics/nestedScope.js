08d001f69764b81147ac6d7e57bb928c2eee6f62
harshl36 <harshlavania36@gmail.com>
2024-06-20 23:50:50 +0530

    Nested function implementation for learning about scopes

---
+++ 03_basics/nestedScope.js
@@ -0,0 +1,38 @@
+// function One() {
+//     const username="Harsh"
+
+//     function Two() {
+//         const website = "Youtube"
+//         console.log(username);
+//     }
+//     // console.log(website);
+
+//     Two()
+// }
+// One()
+
+if (true){
+
+    const username = "Harsh"
+    if (username==="Harsh") {
+        const website = "youtube"
+        // console.log(username+website);
+    }
+    // console.log(website);  throw error
+}
+// console.log(username);
+
+
+// ________________INTRESTING____________________________
+
+console.log(addone(5))
+function addone(num) {
+    return num+1
+}
+// console.log(addone(5))
+
+
+const addtwo = function (num) {    //it is also a funxtion but also called EXPRESSIONS
+    return num+2
+}
+addtwo(5)
