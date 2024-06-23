2105e63c667edc4976c91cd6425ccfba711f6e17
harshl36 <harshlavania36@gmail.com>
2024-06-24 00:15:25 +0530

    While and DoWhile loop

---
+++ 05_iterations/Two.js
@@ -0,0 +1,22 @@
+// While loop
+
+let index = 0
+while (index <=10) {
+    // console.log(`Value of index: ${index}`);
+    index = index +2;
+}
+
+
+let myArray = [`Flash`, `Doreamon`, `Nobita`];
+let arr = 0
+while (arr < myArray.length) {
+    // console.log(myArray[arr]);
+    arr++;
+}
+
+let score =  11
+
+do {
+    console.log(score);
+    score++
+} while (score <=10);
